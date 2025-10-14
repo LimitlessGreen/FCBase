a505e1771aa1f8ba146f97e3e855a358174e7eb8
Jairus <limitlessgreen@outlook.de>
2025-10-14 03:03:29 +0200

    fix: use HEAD branch for edit links

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const GITHUB_REPO_URL = "https://github.com/FCBase/FCBase";
-export const GITHUB_EDIT_BASE_URL = `${GITHUB_REPO_URL}/edit/main`;
+export const GITHUB_EDIT_BASE_URL = `${GITHUB_REPO_URL}/edit/HEAD`;
