fc999b173a7356a33c3bc0186f4d4c535c3bce98
Jairus <limitlessgreen@outlook.de>
2025-10-14 03:00:49 +0200

    feat: show latest commit in footer

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_GIT_COMMIT_HASH: string;
+  readonly PUBLIC_GIT_COMMIT_HASH_FULL: string;
+  readonly PUBLIC_GIT_COMMIT_AUTHOR: string;
+  readonly PUBLIC_GIT_COMMIT_DATE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
