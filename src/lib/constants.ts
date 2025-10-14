2344781e1f7a0dfb57659e2f7b55ed588971200e
Jairus <limitlessgreen@outlook.de>
2025-10-14 04:04:02 +0200

    fix(data): repo-link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const GITHUB_REPO_URL = "https://github.com/limitlessgreen/FCBase";
+export const GITHUB_REPO_URL = "https://github.com/LimitlessGreen/FCBase";
 export const GITHUB_EDIT_BASE_URL = `${GITHUB_REPO_URL}/edit/HEAD`;
